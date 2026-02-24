5b2c29d48c29aef9f6f3c241fb25a41d28cfc152
ostendawn <ostendawn@HN1j>
2026-02-24 19:39:32 +0800

    善用asp-for

--- MvcMovie/Models/MovieGenreViewModel.cs
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -7,6 +7,8 @@ public class MovieGenreViewModel
 {
     public List<Movie>? Movies { get; set; }
     public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }  // 用於 強型別View 的 asp-for
+    public string? SearchString { get; set; }  // 用於 強型別View 的 asp-for
 
 }
 
